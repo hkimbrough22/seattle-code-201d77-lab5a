cff92027eee24f0179e91f358c14f243f4afdc4e
Haustin Kimbrough <haustin.kimbrough@hotmail.com>
2021-07-16 14:41:31 -0700

    added q3

--- app.js
+++ app.js
@@ -12,7 +12,6 @@ function sum(a, b) { //eslint-disable-line
   let sum = a + b
   // The sum of 4 and 7 is 11.
   let string = 'The sum of ' + a + ' and ' + b + ' is ' + sum + '.';
-  console.log(string);
   return [sum, string];
 }
 
@@ -53,12 +52,21 @@ IMPORTANT DETAIL: You may not use the arithmetic operators + and * in this funct
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumAndMultiply() function and see if the test passes.*/
 
 // Write your code here
+
 function sumAndMultiply(a, b, c) { //eslint-disable-line
+  let sum1 = sum(a, b)[0];
+  let sum2 = sum(sum1, c)[0];
+  let product1 = multiply(a, b)[0];
+  let product2 = multiply(product1, c)[0];
+  let string1 = + a + ' and ' + b + ' and ' + c + ' sum to ' + sum2 + '.';
+  let string2 = 'The product of ' + a + ' and ' + b + ' and ' + c + ' is ' + product2 + '.';
+
 // your previous function sum returns an array. let answer = sum(10, 5)[0];
+return [sum2, product2, string1, string2];
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
